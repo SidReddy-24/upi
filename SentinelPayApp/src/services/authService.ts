30dad52df3039ef23f6ae905046e5f024d578009
pranaykadam <2024.pranayk@isu.ac.in>
2026-07-23 01:38:34 +0530

    Network: Change base API and WebSocket URL to LAN network IP (192.168.1.5)

--- SentinelPayApp/src/services/authService.ts
+++ SentinelPayApp/src/services/authService.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import ReactNativeBiometrics from 'react-native-biometrics';
 
-export const API_BASE_URL = 'http://10.0.2.2:8000/api/v1';
+export const API_BASE_URL = 'http://192.168.1.5:8000/api/v1';
 const API_KEY = 'fs_demo_key_001';
 
 const rnBiometrics = new ReactNativeBiometrics();
