11d59b805c995788afddc8d993c9c4f41522d70b
pranaykadam <2024.pranayk@isu.ac.in>
2026-07-23 01:14:35 +0530

    Fix TextEncoder Render Error by adding global polyfill

--- SentinelPayApp/index.js
+++ SentinelPayApp/index.js
@@ -2,6 +2,19 @@
  * @format
  */
 
+if (typeof global.TextEncoder === 'undefined') {
+  global.TextEncoder = class TextEncoder {
+    encode(string) {
+      const utf8 = unescape(encodeURIComponent(string));
+      const arr = new Uint8Array(utf8.length);
+      for (let i = 0; i < utf8.length; i++) {
+        arr[i] = utf8.charCodeAt(i);
+      }
+      return arr;
+    }
+  };
+}
+
 import {AppRegistry} from 'react-native';
 import App from './src/App';
 import {name as appName} from './app.json';
